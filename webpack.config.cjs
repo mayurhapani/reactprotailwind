40c942c941ff41963370a70d0669da0b2e9e41f2
Mayur Hapani <hapanimayuur@gmail.com>
2024-05-19 16:43:15 +0530

    trying to deploy

---
+++ webpack.config.cjs
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/main.jsx",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};

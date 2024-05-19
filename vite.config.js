1b88a00bec794ca3b598b192d73ac9c8b11a1ef8
Mayur Hapani <hapanimayuur@gmail.com>
2024-05-19 21:47:39 +0530

    setup vite for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "public", // Ensure this path is correct
+    outDir: "dist", // Ensure this path is correct
   },
 });
